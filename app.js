bd78e56d733fbb002844ceac1a14df73efc37293
harshit <harshit.tiwari2020@vitbhopal.ac.in>
2022-09-19 13:53:55 +0530

    check box post request done

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const ejs = require('ejs');
-const date = require(__dirname+'/date.js');
+const date = require(__dirname + '/date.js');
+
+// let item = [];
+let workItem = [];
+
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/TODOlistDB');
 
 const app = express();
 
-let item = [];
+
+var Schema = mongoose.Schema;
+
+var TodoSchema = new Schema({
+
+    name: String
+});
+
+// Compile model from schema
+var todoModel = mongoose.model('item', TodoSchema);
+
+const item1 = new todoModel({
+    name: "Web Development"
+});
+
+const item2 = new todoModel({
+    name: "GFG CPI"
+});
+
+const item3 = new todoModel({
+    name: "Android Development"
+});
+
+
+let defaultArr = [item1, item2, item3];
+
+// todoModel.insertMany(defaultArr, function (err) { 
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("successfully todo list items added");
+//     }
+//  });
+
+
+
+
+
+
 // setting it up for view folder so it can look out through views folder
 app.set('view engine', 'ejs');
 
@@ -16,25 +61,60 @@ app.use(express.static("public"));
 
 
 app.get("/", function (req, res) {
+    let day = date.getDate();
 
-   let day = date.getDate();
+    todoModel.find(function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            // render ejs
+            res.render("list", { kindOfDay: day, newItem: result });
+        }
+    });
 
-    // render ejs
-    res.render("list", { kindOfDay: day, newItem: item });
+
 
 });
+
+app.get("/work", function (req, res) {
+
+    res.render("list", { kindOfDay: "Work", newItem: workItem });
+})
+
 
 app.post("/", function (req, res) {
 
     let items = req.body.task;
 
-    
-    item.push(items);
-    
+    if (req.body.posting === "Work") {
+        workItem.push(items);
+        res.redirect("/work");
+    } else {
+        console.log(req.body);
+        const item = new todoModel({
+            name: items
+        })
+        item.save();
+        // item.push(items);
+        res.redirect("/");
+    }
 
-    res.redirect("/");
 
 });
+
+app.post("/delete", function (req, res) { 
+    console.log(req.body.check);
+    let itemId = req.body.check;
+
+    todoModel.deleteOne({_id: itemId}, function (err) { 
+        if(err){
+            console.log(err);
+        }
+        res.redirect("/");
+     })
+
+ });
+
 
 
 
